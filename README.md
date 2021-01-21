93efd6b20e434a092ac5e0ab2fb881efca3a35df
ericwang2006 <erciwang2006@gmail.com>
2021-01-22 00:21:06 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# 修复天邑路由器某些端口不能转发的问题
+
+## 简介
+
+TIANYI(天邑)路由器的端口转发存在bug，某些合法端口无法转发(比如60747)
+原因就是判断端口是否合法的正则表达式存在错误，这个脚本采用hotfix的方式修复。
+
+# 重要提示
+
+- 本脚本仅用于测试和学习研究，禁止用于商业用途
+- 本人不能保证其合法性，准确性，完整性和有效性
+- 本人不承担使用此脚本带来的任何直接或者间接的损失
+- 不提供任何形式的技术支持
+- 请根据自己的承受能力选择是否使用
+
+# 食用方法
+
+1. 开启telnet
+	`curl http://wifieasy.cn/getDiagResult.json?step=-1&action=utelnetd -l /bin/sh`
+	或者直接用浏览器访问上面网址
+
+2. 保证连接互联网，使用任意telnet客户端连接路由器，执行命令
+	`curl -s -k https://cdn.jsdelivr.net/gh/ericwang2006/tianyi_adv_nat_fix/ty_fix.sh |sh -s hotfix`
+3. 恢复
+	直接重启路由器或者用telnet连接路由器，执行命令
+	`curl -s -k https://cdn.jsdelivr.net/gh/ericwang2006/tianyi_adv_nat_fix/ty_fix.sh |sh -s rollback`
+
+4. 已知问题
+	目前仅在TY300路由器新web界面固件下测试过，其它固件或者其他型号机型没有测试过。
---
+++ ty_fix.sh
@@ -0,0 +1,60 @@
+#/bin/sh
+
+# 修复天邑路由器某些端口不能转发的问题
+# 本脚本仅用于测试和学习研究，禁止用于商业用途
+# 本人不能保证其合法性，准确性，完整性和有效性
+# 本人不承担使用此脚本带来的任何直接或者间接的损失
+
+
+rollback() {
+	mount | grep '/webs/ujs/router.js' >/dev/null
+	if [ $? -eq 0 ]; then
+		umount /webs/ujs/router.js
+	fi
+	dir="/tmp/routerjs"
+
+	if [ -d "$dir" ]; then
+		rm -rf $dir
+	fi
+}
+
+hotfix() {
+	rollback
+
+	dir="/tmp/routerjs"
+	mkdir $dir
+	cp /webs/ujs/router.js $dir/
+	num=$(cat $dir/router.js | grep -n 'function CheckPort(port)' | cut -d ":" -f 1)
+	num=$((num + 1))
+	# 此行注释
+	sed -i "$num {s/^/\/\//}" $dir/router.js
+	# 正确的端口号正则表达式
+	sed -i "$num avar reg =/^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/\r" $dir/router.js
+	num=$((num + 1))
+	# 插入tab
+	sed -i "$num {s/^/\t/}" $dir/router.js
+	echo 'download busybox-armv7l...'
+	curl -o $dir/busybox-armv7l http://192.168.2.25/busybox-armv7l && chmod +x $dir/busybox-armv7l
+	if [ $? -eq 0 ]; then
+		$dir/busybox-armv7l mount --bind $dir/router.js /webs/ujs/router.js
+		echo 'fix sucess'
+	else
+		echo 'download busybox-armv7l fail'
+	fi
+}
+
+main() {
+	action="$1"
+	case "${action}" in
+	hotfix)
+		hotfix
+		;;
+	rollback)
+		rollback
+		;;
+	*) ;;
+
+	esac
+}
+
+main "$@"
